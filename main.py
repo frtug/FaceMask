11d12d81b5382a28be180ac2fe38d91e66586dd0
Abhishek Sharma <abhishek11703182@gmail.com>
2020-10-30 05:15:09 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,92 @@
+
+from keras.models import Sequential
+from keras.layers import Dense,Activation,Flatten,Dropout
+from keras.layers import Conv2D,MaxPooling2D
+from keras.callbacks import ModelCheckpoint
+
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
+import tensorflow as tf
+import cv2
+import numpy as np
+
+
+DIRECTORY = "dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+
+print("[INFO] loading images...")
+
+data = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(DIRECTORY, category)
+    for img in os.listdir(path):
+    	img_path = os.path.join(path, img)
+    	image = load_img(img_path, target_size=(224, 224))
+    	image = img_to_array(image)
+    	image = preprocess_input(image)
+
+            
+
+    	data.append(image)
+    	labels.append(category)
+
+# perform one-hot encoding on the labels
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+
+
+model=Sequential()
+
+model.add(Conv2D(200,(3,3),input_shape=data.shape[1:]))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The first CNN layer followed by Relu and MaxPooling layers
+
+model.add(Conv2D(100,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+#The second convolution layer followed by Relu and MaxPooling layers
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+#Flatten layer to stack the output convolutions from second convolution layer
+model.add(Dense(50,activation='relu'))
+#Dense layer of 64 neurons
+model.add(Dense(2,activation='softmax'))
+#The Final layer with two outputs for two categories
+
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+
+
+
+(train_data, test_data, train_target, test_target) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+model.fit(train_data,train_target, epochs=10,validation_split=0.2)
+
+test_loss, test_acc = model.evaluate(test_data,  test_target, verbose=2)
+print('\nTest accuracy:', test_acc)
+
+
+plt.plot(history.history['loss'],'r',label='training loss')
+plt.plot(history.history['val_loss'],label='validation loss')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+
+plt.plot(history.history['accuracy'],'r',label='training accuracy')
+plt.plot(history.history['val_accuracy'],label='validation accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
